a4eae9ae44d32f6e8900cfa96cdbea63464855fc
KonstantinLeschenko <mypost.kh@gmail.com>
2023-10-06 20:50:54 +0300

    my_app_bar added

--- lib/Widgets/my_menu_container.dart
+++ lib/Widgets/my_menu_container.dart
@@ -4,8 +4,10 @@ import 'package:unlock/Data/constance.dart';
 // ignore: must_be_immutable
 class MyMenuContainer extends StatelessWidget {
   String assetName;
+  // ignore: prefer_typing_uninitialized_variables
+  final function;
 
-  MyMenuContainer({super.key, required this.assetName});
+  MyMenuContainer({super.key, required this.assetName, required this.function});
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +15,22 @@ class MyMenuContainer extends StatelessWidget {
 
     return Row(
       children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(15, 15, 0, 0),
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Constance().appColor,
-                  borderRadius: BorderRadius.circular(10)),
-              child: ImageIcon(
-                AssetImage(
-                  assetName,
-                ),
-                color: Colors.white,
-                size: screenWidth / 5 + 15,
-              )),
+        GestureDetector(
+          onTap: function,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 15, 0, 0),
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Constance().appColor,
+                    borderRadius: BorderRadius.circular(10)),
+                child: ImageIcon(
+                  AssetImage(
+                    assetName,
+                  ),
+                  color: Colors.white,
+                  size: screenWidth / 5 + 15,
+                )),
+          ),
         ),
       ],
     );
